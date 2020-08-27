a064e0a639edc34855884da2e3783539c51cacd5
Ubaada <theubaada@gmail.com>
2020-08-27 16:43:19 +1200

    Delete bird sighting from db using button in front end.
    A php file gets key of the row to delete in mysql table.
    Then the image file is deleted associated with the id.
    The js function delete_bird calls the php file.
    Corrected some form validation.

---
+++ www/delete-bird.php
@@ -0,0 +1,32 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+try{
+	$id = $_GET["id"];
+	$db_host   = '127.0.0.1:3306';
+	$db_name   = 'fvision';
+	$db_user   = 'webuser';
+	$db_passwd = 'insecure_db_pw';
+
+	$pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
+
+	$pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
+
+	$stmt = $pdo->prepare("DELETE FROM birds WHERE id=:id");
+	$stmt->bindParam(':id', $id);
+	if ($stmt->execute()=== TRUE) {
+		//successfully deleted row
+		//Now delete its image.
+		unlink('/vagrant/www/birdimages/' . $id . '.jpg');
+		echo "200";
+	} else {
+		echo "500";
+	}
+} catch(PDOException $e) {
+	echo $e;
+}
+
+?>
