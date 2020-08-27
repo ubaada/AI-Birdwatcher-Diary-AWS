76306d5ec2986442ced04b7d224995aff27cfe0e
Ubaada <theubaada@gmail.com>
2020-08-27 15:59:16 +1200

    Show stored bird info clicking the boxes.
    A modal (box) is shown when a previously saved bird is selected, showing its full details.
    It presents the option to delete the entry (not yet implemented).

--- www/get_all.php
+++ www/get_all.php
@@ -1,5 +1,5 @@
 <?php
- 
+
 $db_host   = '127.0.0.1:3306';
 $db_name   = 'fvision';
 $db_user   = 'webuser';
@@ -12,7 +12,22 @@ $pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
 $q = $pdo->query("SELECT * FROM birds");
 
 while($row = $q->fetch()){
-  echo '<div class="card hoverable"><div class="row"><div class="col s8"><div class ="truncate grey-text text-darken-1" style="padding-left:10px;padding-top: 14px;">' . $row["common_name"] . '</div></div><div class="col s4 right-align"><img style ="max-width:50px;height:50px;margin:auto;" src="birdimages/'. $row["id"] .'.jpg"/></div></div>	</div>';
+	$s ="'";
+	echo '<div class="card hoverable" onclick="show_dialog('
+	. $s . $row["id"] . $s . ',' 
+	. $s . $row["scientific_name"] . $s . ',' 
+	. $s . $row["common_name"] . $s . ',' 
+	. $s . $row["sighting_time"] . $s . ',' 
+	. $s . $row["location"] . $s  .
+	')">
+	<div class="row"><div class="col s8">
+	<div class ="truncate grey-text text-darken-1" style="padding-left:10px;padding-top: 14px;">'
+	. $row["common_name"] . 
+	'</div></div><div class="col s4 right-align">
+	<img style ="max-width:50px;height:50px;margin:auto;" src="birdimages/'. $row["id"] .'.jpg"/>
+	</div>
+	</div>
+	</div>';
 }
 
 ?>
